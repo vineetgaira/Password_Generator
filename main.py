db843f605cca47b584bde904bb5f605f1465e0a3
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-02 12:21:26 +0530

    Create functions structure

---
+++ main.py
@@ -0,0 +1,14 @@
+def user_parameters():
+    length=int(input("Enter length of password :"))
+
+def password_generate():
+    pass
+
+def password_display():
+    pass
+
+def user_feedback():
+    pass
+
+def update_password():
+    pass
