c7c5f691cab0880e3dc96fb69694182eb2b86f8d
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-03 11:48:27 +0530

    Add password logic

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import secrets
 import pyperclip
 
 GREEN = "\033[32m"
-YELLOW = "\033[33m"
 BLUE = "\033[34m"
 RED = " \033[31m"
 RESET = "\033[0m"
 BOLD_START = "\033[1m"
 BOLD_END = "\033[0m"
+MAX_LENGTH = 64
+MIN_LENGTH = 1
+WEAK = (string.ascii_lowercase + string.ascii_uppercase)
+MEDIUM = (string.ascii_lowercase + string.ascii_uppercase + string.digits)
+STRONG = (string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation)
+
+
+
+
 
 def display_menu():
     print(f"{BOLD_START}{BLUE}Welcome! to password generator.{BOLD_END}\n" 
@@ -26,13 +34,11 @@ def get_user_choice():
     while True:
         try:
             length=int(input(f"{BOLD_START}{BLUE}Enter the length of the password(integer):{BOLD_END}"))
-            if 1 <= length <= 15:
-                pass
-            else:
-                print(f"{RED}Please enter a valid choice! Between 1-15.")
+            if not (MIN_LENGTH <= length <= MAX_LENGTH):
+                print(f"{RED}Please enter a valid choice! Between 1-64.")
                 continue
         except ValueError:
-            print(f"{RED}Please enter a integer between 1-15.{RESET}")
+            print(f"{RED}Please enter a integer between 1-64.{RESET}")
             continue
         while True:
             try:
@@ -45,7 +51,21 @@ def get_user_choice():
                 print(f"{RED}Please enter a valid choice! Look at the menu.{RESET}")
         
 def password_logic():
-    pass
+    password = ""
+    user_choice,length=get_user_choice()
+    if user_choice==1:
+        for _ in range(length):
+            password += secrets.choice(WEAK)
+        return password
+    elif user_choice==2:
+        for _ in range(length):
+            password += secrets.choice(MEDIUM)
+        return password
+    elif user_choice==3:
+        for _ in range(length):
+            password += secrets.choice(STRONG)
+        return password
+        
 def password_generator():
     pass
 
@@ -54,10 +74,6 @@ def display_password():
 def copy_to_clipboard():
     pass
 
-display_menu()
-get_user_choice()
-            
-            
 
                 
 
