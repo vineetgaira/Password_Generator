8fe945f049490adf9d617f9dd0360d47727351f8
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-03 08:59:01 +0530

    Add menu

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-def user_parameters():
-    length=int(input("Enter length of password :"))
+import string
+import argparse
+import secrets
+import pyperclip
 
-def password_generate():
-    pass
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+RESET = "\033[0m"
+BOLD_START = "\033[1m"
+BOLD_END = "\033[0m"
 
-def password_display():
-    pass
+def display_menu():
+    print(f"{BOLD_START}{YELLOW}Welcome! to password generator.{BOLD_END}\n" 
+    f"{YELLOW}You can genrate any type of password of any length.\n"
+    "You can use symbols, numbers, letters.\n"
+    "You can choose any of the following, and combination, or all of them.\n")
+    print(f"{BOLD_START}Select a password strength level.{BOLD_END}\n"
+          f"{YELLOW}1 : Weak\n" \
+          "2 : Meduim\n" \
+          f"3 : Strong{RESET}")
+    
 
-def user_feedback():
-    pass
 
-def update_password():
-    pass
+display_menu()
